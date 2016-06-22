6fa21c1e3ae84b0644dfe2b9a6ea5b55794904aa
Andreia Gaita <shana@spoiledcat.net>
2016-06-22 17:52:30 +0200

    Don't need this either

--- TrackingCollection/TrackingCollection/TrackingCollection.cs
+++ TrackingCollection/TrackingCollection/TrackingCollection.cs
@@ -17,7 +17,6 @@ using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace GitHub.Collections
 {
